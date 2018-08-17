d689fa012d3b8c40052893f66b398f3baeb89db4
Fady ZAKHARIA <fady.zakharia@isae.edu.lb>
2018-08-17 14:05:37 +0300

    Fist Commit

---
+++ projetcameneons/Agora.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetcameneons;
+
+/**
+ *
+ * @author FADY
+ */
+public class Agora {
+    private Couleur couleur1;
+    private Couleur couleur2;
+}
